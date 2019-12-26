87d45663b4fb56de30340c4079b46731fe4b49f4
Lisandro <lcaballero@urbano.com.ar>
2019-12-26 18:02:00 -0300

    Trabajando con ajax

--- src/Controller/BajarPIController.php
+++ src/Controller/BajarPIController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BajarPIController extends AbstractController
@@ -17,5 +18,17 @@ class BajarPIController extends AbstractController
         return $this->render('bajar_pi/index.html.twig', [
             'controller_name' => 'BajarPIController',
         ]);
+    }
+
+    /**
+     * @IsGranted("IS_AUTHENTICATED_FULLY")
+     *  @Route("/get-piezas-U3", name="get-piezas-U3", methods={"POST"}, options={"expose"=true})
+     */
+    public function getPiezasU3()
+    {
+        sleep(5);
+        $response = new Response(json_encode(array('name' => "lisandro")));
+        $response->headers->set('Content-Type', 'application/json');
+        return $response;
     }
 }

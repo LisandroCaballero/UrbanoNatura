12e37d3744ef46e67d630477022fa4df81c35174
Federico Garcia <fgarcia@urbano.com.ar>
2020-03-08 14:06:58 -0300

    se reparado migration despues de romper la db

--- src/Migrations/Version20200308170439.php
+++ src/Migrations/Version20200308170439.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200308143656 extends AbstractMigration
+final class Version20200308170439 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,7 +22,8 @@ final class Version20200308143656 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE piezas (id INT AUTO_INCREMENT NOT NULL, id_reg INT NOT NULL, lote INT NOT NULL, sucursal VARCHAR(5) NOT NULL, shipper INT NOT NULL, campo1 VARCHAR(255) NOT NULL, campo2 VARCHAR(255) NOT NULL, campo11 VARCHAR(255) NOT NULL, file_nombre VARCHAR(255) NOT NULL, datetime DATETIME NOT NULL, id_usuario INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE piezas (id INT AUTO_INCREMENT NOT NULL, id_reg INT NOT NULL, lote BIGINT NOT NULL, sucursal VARCHAR(5) NOT NULL, shipper INT NOT NULL, campo1 VARCHAR(255) NOT NULL, campo2 VARCHAR(255) NOT NULL, campo11 VARCHAR(255) NOT NULL, file_nombre VARCHAR(255) NOT NULL, datetime DATETIME NOT NULL, id_usuario INT NOT NULL, estado VARCHAR(2) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema) : void
@@ -31,5 +32,6 @@ final class Version20200308143656 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('DROP TABLE piezas');
+        $this->addSql('DROP TABLE user');
     }
 }
